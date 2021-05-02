0564b977cdad3c0d49969cd56603a239f14b98bf
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-05-02 16:50:16 +0530

    Added all string functions

--- src/test_main.c
+++ src/test_main.c
@@ -1,8 +1,24 @@
 #include "functions.h"
 
 int main() {
-    char *name = "Pranoy";
+    char *name = "Pranoy Jayaraj";
     int len = my_strlen(name);
-    printf("%d\n", len);
+    printf("Str length: %d\n", len);
+    my_puts(name);
+    
+    char* name2 = "Jayaraj";
+    if (my_substring(name, name2) == 0) {
+        my_puts("Substring found");
+    }
+
+    char* rev_name = NULL;
+    rev_name = my_strrev(name);
+    my_puts("Reversed string");
+    my_puts(rev_name);
+
+    if (my_strcmp("Jayaraj", "Jayaraj") == 0) {
+        my_puts("Str cmp passed");
+    }
+
     return 0;
 }
