1f7a579d5cbd95d6b86459b4ff7e0bd45d5b2f78
Pranoy Jayaraj <Pranoy.Jayaraj@amd.com>
2021-05-02 13:21:19 +0530

    updated string functions

--- include/functions.h
+++ include/functions.h
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*string functions*/
 void my_puts(char* str);
 int my_strlen(const char* str);
 char* my_strrev(char* str);
 int my_strcmp(char* str1, char* str2);
-
+char* my_strcat(const char* str1, const char* str2);
 
 
--- src/functions.c
+++ src/functions.c
@@ -47,3 +47,22 @@ int my_strcmp(char* s1, char* s2) {
 
     return ret_val;
 }
+
+char* my_strcat(const char* s1, const char* s2) {
+    char* s = NULL;
+    int l1 = 0, l2 = 0, l = 0, i = 0;
+    l1 = my_strlen(s1);
+    l2 = my_strlen(s2);
+    s = (char*)malloc((l1 + l2) * sizeof(char));
+    while (i < l1) {
+        *(s + i) = *(s1 + i);
+        i++;
+    }
+    i = 0;
+    while (i<l2) {
+        *(s + l1 + i) = *(s2 + i);
+        i++;
+    }
+
+    return s;
+}
